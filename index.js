e95a3c85cffe793d22e6e7cd5c653f926f293776
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-04-19 15:02:51 -0400

    commit tree 2795887deef770d56d22dda2b0507c25404e63eb

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your code here
+// const postInfo = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle",
+//   }
+// const configObj = {method: 'Post',
+//     headers:{ 'Content-Type': 'application/json',
+//                 "Accept":'application.json' },
+//     body: JSON.stringify(postInfo)/*your data here*/}
+// const url = 'http://localhost:3000/dogs'
+
+// fetch(url, configObj ).then((res)=>res.json()).then(data=>console.log(data))
+
+
+function submitData(name, email){
+
+   let postInfo = {'name': name, 'email': email}
+   let configOBJ = {method: 'Post', headers: {'Content-Type':'application/json', 'Accept':'application/json'},
+    body: JSON.stringify(postInfo)
+}
+
+    return fetch('http://localhost:3000/users', configOBJ).then(res=>res.json()).then(data=>document.querySelector('body').textContent = `${data.id}`).catch(error=>document.querySelector('body').textContent = `${error.message}`)
+
+}
